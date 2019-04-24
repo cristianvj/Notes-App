4d8fe07a9efb107c27974dabc10b95f2b2c08a08
cristianvj <cristiandavid.vj@gmail.com>
2019-04-23 23:03:09 -0500

    Configuracion de handlebars, plantilla inicial de vistas

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,12 @@
-const router = require('express').Router()
+const express = require('express')
+const router= express.Router()
 
 router.get('/', (req, res)=>{
-    res.send('Index')
+    res.render('index')
 })
 
 router.get('/about', (req, res)=>{
-    res.send('About')
+    res.render('about')
 })
 
 module.exports = router
