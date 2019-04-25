6f936c8f07a5e674d6a852aab75f10785240ecfb
cristianvj <cristiandavid.vj@gmail.com>
2019-04-25 12:48:56 -0500

    Creación del modelo de la DB, y guardar nueva nota del CRUD

--- src/routers/notes.js
+++ src/routers/notes.js
@@ -1,5 +1,34 @@
 const router = require('express').Router()
 
+const Note = require('../models/Note')
+
+router.get('/notes/add', (req, res)=>{
+    res.render('notes/new-note')
+})
+
+router.post('/notes/new-note', async (req, res)=>{
+    const {title, description} = req.body
+    const errors = []
+    if(!title){
+        errors.push({text: 'Please write a Title'})
+    }
+    if(!description){
+        errors.push({text: 'Please write a Description'})
+    }
+    if(errors.length > 0){
+        res.render('notes/new-note',{
+            errors,
+            title,
+            description
+        })
+    }else{
+        //Almacenamos en base de datos cuando ingrese titulo y descripción
+        const newNote = new Note({title, description})
+        await newNote.save()
+        res.redirect('/notes')
+    }
+})
+
 router.get('/notes', (req, res)=>{
     res.send('Notas de la base de datos')
 })
