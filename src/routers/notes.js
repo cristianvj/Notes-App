db2b0760cb08b1bc28345594ade08daf25e23ecc
cristianvj <cristiandavid.vj@gmail.com>
2019-05-26 23:08:59 -0500

    Solicita autenticación para ingresar a las notas cuandop no se ha logueado

--- src/routers/notes.js
+++ src/routers/notes.js
@@ -2,11 +2,13 @@ const router = require('express').Router()
 
 const Note = require('../models/Note')
 
-router.get('/notes/add', (req, res)=>{
+const {isAuthenticated} = require('../helpers/auth')
+
+router.get('/notes/add', isAuthenticated, (req, res)=>{
     res.render('notes/new-note')
 })
 
-router.post('/notes/new-note', async (req, res)=>{
+router.post('/notes/new-note', isAuthenticated,async (req, res)=>{
     const {title, description} = req.body
     const errors = []
     if(!title){
@@ -30,24 +32,24 @@ router.post('/notes/new-note', async (req, res)=>{
     }
 })
 
-router.get('/notes', async (req, res)=>{
+router.get('/notes', isAuthenticated,async (req, res)=>{
     const notes = await Note.find().sort({date:'desc'})
     res.render('notes/all-notes', {notes})
 })
 
-router.get('/notes/edit/:id', async (req, res)=>{
+router.get('/notes/edit/:id', isAuthenticated, async (req, res)=>{
     const note = await Note.findById(req.params.id)
     res.render('notes/edit-note',{note})
 })
 
-router.put('/notes/edit-note/:id', async (req, res)=>{
+router.put('/notes/edit-note/:id', isAuthenticated, async (req, res)=>{
     const {title, description} = req.body
     await Note.findByIdAndUpdate(req.params.id, {title, description})
     req.flash('success_msg', 'Note Upload Successfuly')
     res.redirect('/notes')
 })
 
-router.delete('/notes/delete/:id', async (req, res)=>{
+router.delete('/notes/delete/:id', isAuthenticated, async (req, res)=>{
     await Note.findByIdAndDelete(req.params.id)
     req.flash('success_msg', 'Note Deleted Successfuly')
     res.redirect('/notes')
